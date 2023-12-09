4f94d9c652e3c8f074ee70aeac7ff6d312f01043
Jesus Hernandez <jesushdzusa@gmail.com>
2023-12-09 11:37:57 -0600

    first commit

---
+++ housewebsite/gpt_endpoints.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+from openai import OpenAI
+import openai
+
+client = OpenAI()
+#ENDPOINT TEXT TO AUDIO
+from openai import OpenAI
+client = OpenAI()
+
+def textToAudio():
+    speech_file_path = Path(__file__).parent/"speech.mp3"
+    response = openai.audio.speech.create(
+    model =  "tts-1",
+    voice =  "alloy",   
+    input = "Gongon is late, wakeup and get up! we need to go to supermarket"
+    )
+    return response.stream_to_file(speech_file_path)
+
+#ENDPOINT COMPLETITION
+
+
+def consultant(topic):
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role":"system",
+         "content":"You are Doctor Gregory House from the medical drama serie and you like to give briefly and sarcastic opinions about posible causes of health symptoms and address the consultants to visit a real doctor."},
+        {"role":"user","content":f"{topic}"}
+    ],
+    #temperature=1.0,
+   # max_tokens=100
+    )
+    return completion.choices[0].message
+
+
+
+#ENDPOINT CREATE IMAGES
+
+def imageGenerator():
+    response =  client.images.generate(
+        model =  "dall-e-3",
+        prompt= """create a cook recipe instructions list in english language with an image: Bring the rice to a boil in a large pot of water and simmer for 30 minutes or until tender.
+
+            """,
+        size = "1024x1024",
+        quality= "standard",
+        n=1)
+    return response.data[0].url
